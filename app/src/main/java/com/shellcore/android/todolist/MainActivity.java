f92e993df57e523326cdd1dfe1ac8f1aec00aaa2
Cesar Morales Garduño <shell.one.core@gmail.com>
2017-06-20 10:20:53 -0500

    Se creó la vista principal con la lista de tareas.

--- app/src/main/java/com/shellcore/android/todolist/MainActivity.java
+++ app/src/main/java/com/shellcore/android/todolist/MainActivity.java
@@ -1,13 +1,38 @@
 package com.shellcore.android.todolist;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Componentes
+    @BindView(R.id.rec_main)
+    RecyclerView recMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setupRecyclerView();
+    }
+
+    @OnClick(R.id.btn_add)
+    public void onClickBtnAdd() {
+
+    }
+
+    private void setupRecyclerView() {
+        recMain.setLayoutManager(new LinearLayoutManager(this));
+        recMain.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        recMain.setHasFixedSize(true);
     }
 }
